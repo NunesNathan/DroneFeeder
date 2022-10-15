301e38c9d1ee87bf14120ddebac32b766bd76012
NunesNathan <nunesxd7@hotmail.com>
2022-10-15 01:30:31 -0300

    feat: add constants class

---
+++ src/main/java/com/futuereh/dronefeeder/utils/Constants.java
@@ -0,0 +1,7 @@
+package com.futuereh.dronefeeder.utils;
+
+import java.time.format.DateTimeFormatter;
+
+public class Constants {
+  public static final DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/uuuu HH:mm:ss");
+}
