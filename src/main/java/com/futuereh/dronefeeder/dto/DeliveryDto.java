f6feb270610bf37c5a3521aa3a84d5b4856a412e
NunesNathan <nunesxd7@hotmail.com>
2022-10-16 22:13:43 -0300

    feat: delivery dto

---
+++ src/main/java/com/futuereh/dronefeeder/dto/DeliveryDto.java
@@ -0,0 +1,13 @@
+package com.futuereh.dronefeeder.dto;
+
+public class DeliveryDto {
+  private String deliveryType;
+
+  public String getDeliveryType() {
+    return deliveryType;
+  }
+
+  public void setDeliveryType(String deliveryType) {
+    this.deliveryType = deliveryType;
+  }
+}
