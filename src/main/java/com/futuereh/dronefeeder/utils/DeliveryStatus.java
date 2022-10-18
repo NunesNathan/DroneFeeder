d3dd9513af97c5ceb5da57425617f28853e3276b
NunesNathan <nunesxd7@hotmail.com>
2022-10-18 14:54:12 -0300

    fix: delivery status check return boolean

--- src/main/java/com/futuereh/dronefeeder/utils/DeliveryStatus.java
+++ src/main/java/com/futuereh/dronefeeder/utils/DeliveryStatus.java
@@ -3,7 +3,12 @@ package com.futuereh.dronefeeder.utils;
 public enum DeliveryStatus {
   PROCESSING, SHIPPED, IN_TRANSIT, DELIVERED;
 
-  public static DeliveryStatus isStatus(String text) {
-    return DeliveryStatus.valueOf(text.toUpperCase());
+  public static boolean isStatus(String text) {
+    for (DeliveryStatus stts : DeliveryStatus.values()) {
+      if (stts.toString().equals(text)) {
+        return true;
+      }
+    }
+    return false;
   }
 }
