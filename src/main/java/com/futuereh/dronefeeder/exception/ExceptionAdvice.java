f5fd56b6c250b8f3c657f77b40e3b09a39f137b6
NunesNathan <nunesxd7@hotmail.com>
2022-10-20 09:23:30 -0300

    feat: advice handle bad request exceptions

--- src/main/java/com/futuereh/dronefeeder/exception/ExceptionAdvice.java
+++ src/main/java/com/futuereh/dronefeeder/exception/ExceptionAdvice.java
@@ -1,6 +1,9 @@
 package com.futuereh.dronefeeder.exception;
 
+import com.futuereh.dronefeeder.exception.exceptions.BadRequestDelivery;
 import com.futuereh.dronefeeder.exception.exceptions.BadRequestDrone;
+import com.futuereh.dronefeeder.exception.exceptions.BadRequestStatus;
+import com.futuereh.dronefeeder.exception.exceptions.BadRequestType;
 import com.futuereh.dronefeeder.exception.exceptions.DataError;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,7 +12,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class ExceptionAdvice {
-  @ExceptionHandler({BadRequestDrone.class})
+  @ExceptionHandler({BadRequestDrone.class, BadRequestDelivery.class,
+          BadRequestStatus.class, BadRequestType.class})
   @ResponseStatus(HttpStatus.BAD_REQUEST)
   public DataError handleRecursoExistente(RuntimeException e) {
     return new DataError(e.getMessage());
